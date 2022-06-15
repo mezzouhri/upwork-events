acc85be078e821d2cc993118b5042fa4592cc89d
Mohcine EZZOUHRI <ezzouhri.dev@gmail.com>
2022-06-15 21:06:13 +0200

    first commit

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,6 @@ export default function Home() {
 
   useEffect(() => {
     setEventsToShow(getEventByDate(selectedDate));
-    console.log("eventToShow", eventsToShow);
   }, [selectedDate]);
 
   return (
