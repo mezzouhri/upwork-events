22989031006ff642a7ebe33b1f96d8aaac5e0238
Mohcine EZZOUHRI <ezzouhri.dev@gmail.com>
2022-06-15 21:14:29 +0200

    add EventPage

---
+++ pages/events/[slug].js
@@ -0,0 +1,9 @@
+const EventPage = ({}) => {
+  return <div>Hello</div>;
+};
+
+export default EventPage;
+
+export const getStatisPaths = ({ context }) => {};
+
+export const getStaticProps = ({ context }) => {};
